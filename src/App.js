0fc011f004bb045d22255311141d90d9c1adb596
genti91 <franco.g7@hotmail.com>
2022-09-30 15:56:00 -0300

    chat-app

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  var stream = null;
+
+  navigator.mediaDevice?.getUserMedia({video: true})
+  .then(v => stream = v)
+  .catch(console.error)
+
   return (
     <div className="App">
-
+      <div>Stream:</div>
+      <video ref={stream} autoPlay></video>
     </div>
   );
 }
